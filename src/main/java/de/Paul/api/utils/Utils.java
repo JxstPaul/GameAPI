64137bbe1608825590296d7a0e51056c63cd241b
JxstPaul <124691605+JxstPaul@users.noreply.github.com>
2024-06-02 18:50:52 +0200

    Create Utils.java

---
+++ src/main/java/de/Paul/api/utils/Utils.java
@@ -0,0 +1,9 @@
+package de.Paul.api.utils;
+
+import org.bukkit.ChatColor;
+
+public class Utils {
+    public static String msg(String msg) {
+        return ChatColor.translateAlternateColorCodes('&', msg);
+    }
+}
