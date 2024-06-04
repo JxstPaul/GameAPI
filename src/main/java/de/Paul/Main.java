ba9903eb58aeaaff38e20e5d1482909bc3b709e2
JxstPaul <124691605+JxstPaul@users.noreply.github.com>
2024-06-04 18:12:05 +0200

    Update Main.java

--- src/main/java/de/Paul/Main.java
+++ src/main/java/de/Paul/Main.java
@@ -1,15 +1,18 @@
 package de.Paul;
 
-import de.Paul.api.managers.*;
-import de.Paul.impl.*;
+import de.Paul.impl.menus.KitSelection;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import de.Paul.API.managers.*;
+import de.Paul.impl.*;
 import de.Paul.listeners.*;
 
 public class Main extends JavaPlugin {
     private GamestateManagerImpl gamestateManager;
     private SidebarManagerImpl sidebarManager;
     private TeamManagerImpl teamManager;
+    private MenuManagerImpl menuManager;
     private KitManagerImpl kitManager;
 
     @Override
@@ -19,34 +22,26 @@ public class Main extends JavaPlugin {
         gamestateManager.setState(gamestateManager.getStates().get("LobbyState"));
         sidebarManager = new SidebarManagerImpl();
         teamManager = new TeamManagerImpl();
+        menuManager = new MenuManagerImpl();
 
         getServer().getServicesManager().register(GamestateManager.class, gamestateManager, this, ServicePriority.Highest);
         getServer().getServicesManager().register(SidebarManager.class, sidebarManager, this, ServicePriority.Highest);
         getServer().getServicesManager().register(TeamManager.class, teamManager, this, ServicePriority.Highest);
         getServer().getServicesManager().register(KitManager.class, kitManager, this, ServicePriority.Highest);
+
+        menuManager.registerMenu(new KitSelection(kitManager));
     }
 
     private void registerListeners() {
         getServer().getPluginManager().registerEvents(new PlayerJoin(this), this);
+        getServer().getPluginManager().registerEvents(new InventoryClick(menuManager), this);
     }
 
-    public GamestateManagerImpl getGamestateManager() {
-        return gamestateManager;
-    }
+    public GamestateManagerImpl getGamestateManager() { return gamestateManager; }
+    public SidebarManagerImpl getSidebarManager() { return sidebarManager; }
+    public TeamManagerImpl getTeamManager() { return teamManager; }
+    public MenuManagerImpl getMenuManager() { return menuManager; }
+    public KitManagerImpl getKitManager() { return kitManager; }
 
-    public SidebarManagerImpl getSidebarManager() {
-        return sidebarManager;
-    }
-
-    public TeamManagerImpl getTeamManager() {
-        return teamManager;
-    }
-
-    public KitManagerImpl getKitManager() {
-        return kitManager;
-    }
-
-    public void toggleKits(boolean toggle) {
-        kitManager.toggleKits(toggle);
-    }
+    public void toggleKits(boolean toggle) { kitManager.toggleKits(toggle); }
 }
